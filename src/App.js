d1898b84e80bfaaf6df0c22d566288fd70926a99
djohnsonkc <djohnsonkc@gmail.com>
2017-05-18 09:18:04 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Contacts from './components/Contacts';
+import About from './components/About';
+
+import './index.css';
+
+
+const Routes = () => (
+  <div>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contacts} />
+  </div>
+);
 
 class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-  state = {users: []}
-
-  componentDidMount() {
-    fetch('/api/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
-
   render() {
     return (
-      <div className="App">
-
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <NavBar />
+          <Routes />
+          <Footer />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
+      </Router>
     );
   }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import Contacts from './components/Contacts';
-import About from './components/About';
-
-import './index.css';
-
-
-// const Header = () => <h1>I'm a header</h1>;
-
-const Navigation = () => (
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/contact">Contact</Link>
-  </nav>
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
-
-const Routes = () => (
-  <div>
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/contact" component={Contacts} />
-  </div>
-);
-
-
-
-const App = () => (
-  <Router>
-    <div>
-      <NavBar />
-      <Routes />
-      <Footer />
-    </div>
-  </Router>
-);
-
-render(<App />, document.getElementById('root'));
